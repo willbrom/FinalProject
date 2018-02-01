e980d9770c571e42992cb3f924ecacff7b7f14cc
Ali Sajid <a.sajid@es-saudi.com>
2018-02-01 09:51:43 +0300

    added: passes a joke from 'javalib' to androidlibrary's JokeActivity as an intent on 'press joke' click

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.willbrom.androidlibrary.JokeActivity;
 import com.willbrom.javalib.JokeJava;
 
 
@@ -43,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
 //        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, JokeJava.getJoke(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, JokeJava.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, JokeJava.getJoke());
+        startActivity(intent);
     }
 
 
