c57df1e7807f7303d2b2209d32fc45fb6854fc3e
Ali Sajid <a.sajid@es-saudi.com>
2018-02-01 09:24:09 +0300

    created JokeJava class with public static method that returns a joke.

---
+++ javalib/src/main/java/com/willbrom/javalib/JokeJava.java
@@ -0,0 +1,7 @@
+package com.willbrom.javalib;
+
+public class JokeJava {
+    public static String getJoke() {
+        return "I'm only friends with 25 letters of the alphabet. I don't know Y.";
+    }
+}
