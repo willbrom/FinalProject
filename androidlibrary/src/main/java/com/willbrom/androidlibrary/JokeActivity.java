83409f50020d36aebe1ffaa99cfaf59d371b5968
Ali Sajid <a.sajid@es-saudi.com>
2018-02-01 09:49:58 +0300

    created 'JokeActivity' that get's a joke as an intent and displays it in textView.

---
+++ androidlibrary/src/main/java/com/willbrom/androidlibrary/JokeActivity.java
@@ -0,0 +1,21 @@
+package com.willbrom.androidlibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    TextView jokeTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        jokeTextView = (TextView) findViewById(R.id.joke_textView);
+        String joke = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        jokeTextView.setText(joke);
+    }
+}
